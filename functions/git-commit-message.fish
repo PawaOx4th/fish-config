f4db14a05dcec442b199a09ca2509ca945e96435
Kaen Pawanachai <pawaox4th@gmail.com>
2025-06-09 12:53:59 +0700

    feat(fish): update starship config path and add git commit message function

--- functions/git-commit-message.fish
+++ functions/git-commit-message.fish
@@ -1,6 +1,11 @@
 # Defined via `source`
 function gm --wraps=cls --description 'git commit with MESSAGE'
   git commit -m "$argv[1]"
-  echo  "🐙 Usage commit message :  $argv[1]"
+  
+  echo  "
+  ==================================================
+  🐙 Usage commit message :  $argv[1]
+  ==================================================
+  "
 end
 
---
+++ functions/install-ni.fish
@@ -0,0 +1,6 @@
+# Defined via `source`
+function ni --wraps=cls --description 'fnm use && npm i -g @antfu/ni'
+  fnm use
+  npm i -g @antfu/ni
+end
+
