d3ee33dc2982ae1f242d3ac0828b29bd70b8c678
Kaen Pawanachai <pawaox4th@gmail.com>
2025-06-09 12:56:30 +0700

    feat(fish): rename function ni to install-ni for clarity

--- functions/install-ni.fish
+++ functions/install-ni.fish
@@ -1,5 +1,5 @@
 # Defined via `source`
-function ni --wraps=cls --description 'fnm use && npm i -g @antfu/ni'
+function install-ni --wraps=cls --description 'fnm use && npm i -g @antfu/ni'
   fnm use
   npm i -g @antfu/ni
 end
