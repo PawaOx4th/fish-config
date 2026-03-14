e742cf097fbadc6354586b295d74b53fc772a9e1
Kaen Pawanachai (Pawaox4th) <pawaox4th@email.com>
2026-03-14 19:07:55 +0700

    feat: enhance gsb completion with filtered remote suggestions

--- completions/gsb.fish
+++ completions/gsb.fish
@@ -1,7 +1,20 @@
 # fish completion for gsb
 
+complete -c gsb -f
+
 function __gsb_remote_names
     git remote 2>/dev/null
+end
+
+function __gsb_remote_names_filtered
+    set -l current_token (commandline -ct)
+
+    for remote in (__gsb_remote_names)
+        if test -n "$current_token"; and test "$remote" = "$current_token"
+            continue
+        end
+        echo $remote
+    end
 end
 
 complete -c gsb -s h -l help -d 'Show help message'
@@ -11,5 +24,5 @@ complete -c gsb -l no-auto-fetch -d 'Disable background auto-fetch'
 complete -c gsb -l bg-status -d 'Show background fetch status message'
 complete -c gsb -l no-bg-status -d 'Hide background fetch status message'
 
-# Suggest git remotes as positional argument.
-complete -c gsb -f -a '(__gsb_remote_names)' -d 'Git remote'
+# Suggest git remotes only for the first positional argument.
+complete -c gsb -f -n '__fish_is_nth_token 1' -a '(__gsb_remote_names_filtered)' -d 'Git remote'
